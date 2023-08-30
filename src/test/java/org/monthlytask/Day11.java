8e6df83cba5fe4199ffb97b2e20e74d15696bb16
sdhinesh30 <sdhinesh3008@gmail.com>
2023-08-31 00:17:06 +0530

    Day 11

--- src/test/java/org/monthlytask/Day11.java
+++ src/test/java/org/monthlytask/Day11.java
@@ -4,19 +4,7 @@ import java.util.Arrays;
 
 public class Day11 {
 
-	// Left and Right rotation
-
-	public static void leftRotate(int[] a, int rotate) {
-
-		for (int i = 0; i < rotate; i++) {
-			int first = a[0];
-			for (int j = 1; j < a.length; j++) {
-				a[j - 1] = a[j];
-			}
-			a[a.length - 1] = first;
-		}
-	}
-
+	// Right rotation
 	public static void rightRotate(int[] b, int rotate) {
 
 		for (int i = 0; i < rotate; i++) {
@@ -29,12 +17,8 @@ public class Day11 {
 	}
 
 	public static void main(String[] args) {
-		int[] lRotate = { 10, 20, 30, 40, 50, 60 };
 		int[] rRotate = { 100, 200, 300, 400, 500, 600 };
 		int rotate = 2;
-
-		leftRotate(lRotate, rotate);
-		System.out.println("Left Rotate: " + Arrays.toString(lRotate));
 
 		rightRotate(rRotate, rotate);
 		System.out.println("Right Rotate: " + Arrays.toString(rRotate));
